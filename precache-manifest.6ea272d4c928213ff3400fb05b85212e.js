011aac45bc67b30a778121d5e2b304ebc8ffcccf
andylhw <andylhw12@soongsil.ac.kr>
2020-10-29 17:01:29 +0900

    Updates

---
+++ precache-manifest.6ea272d4c928213ff3400fb05b85212e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507001cd4ea82d61c8485ba22d6688f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c79703555686bec2cbd7",
+    "url": "/static/css/main.1309dabf.chunk.css"
+  },
+  {
+    "revision": "221e143c0c107da6b348",
+    "url": "/static/js/2.7758c111.chunk.js"
+  },
+  {
+    "revision": "c79703555686bec2cbd7",
+    "url": "/static/js/main.8d0b5d44.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
